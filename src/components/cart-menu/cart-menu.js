735df3417580a202bc560545efb22c520d9b481a
Andrew Carrigan <carrigan223@gmail.com>
2021-05-27 01:39:55 -0700

    working on header and cart

---
+++ src/components/cart-menu/cart-menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomButton from '../custombutton/custombutton'
+
+import './cart-menu.styles.scss'
+
+const CartMenu = () => (
+    <div className="cart-dropdown">
+        <div>Hello World</div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+)
+
+export default CartMenu
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firbase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon'
+import CartMenu from '../cart-menu/cart-menu'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
@@ -41,16 +42,18 @@ const Header = ({ currentUser }) => {
                         SIGN IN
                     </Link>
                 )}
-            </div>
                 <CartIcon />
+            </div>
+            {hidden ? null : <CartMenu />}
         </div>
     )
 }
 //connect is a higher order component that takes in the component
 //then we map over the reducer state to get the required slice and
 //inject it as props
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden,
 })
 
 export default connect(mapStateToProps)(Header)

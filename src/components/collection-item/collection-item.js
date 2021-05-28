a506169c77fc209b1233f4955c34824d92f36871
Andrew Carrigan <carrigan223@gmail.com>
2021-05-28 01:58:40 -0700

    adjusted some styling

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CustomButton from '../custombutton/custombutton'
 
 import './collection-item.styles.scss'
 
@@ -12,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted>Add To Cart</CustomButton>
     </div>
 )
 
--- src/view-components/shoppage/shop.js
+++ src/view-components/shoppage/shop.js
@@ -14,11 +14,13 @@ class ShopPage extends React.Component {
 
     render() {
         const { collections } = this.state
-        return (<div className="shop-page">{
-            collections.map(({id, ...otherCollectionProps}) => (
-                <PreviewCollection key={id} {...otherCollectionProps} />
-            ))
-        }</div>)
+        return (
+            <div className="shop-page">
+                {collections.map(({ id, ...otherCollectionProps }) => (
+                    <PreviewCollection key={id} {...otherCollectionProps} />
+                ))}
+            </div>
+        )
     }
 }
 

3b65b4fb2166b3b42cdd8591550784ffb65869e5
Andrew Carrigan <carrigan223@gmail.com>
2021-06-02 00:09:59 -0700

    added some cart function

--- src/components/cart-menu/cart-menu.js
+++ src/components/cart-menu/cart-menu.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import CustomButton from '../custombutton/custombutton'
+import CartItem from '../cart-item/cart-item'
+import { connect } from 'react-redux'
 
 import './cart-menu.styles.scss'
 
-const CartMenu = () => (
+const CartMenu = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div>Hello World</div>
+        <div className="cart-items">
+            {cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartMenu
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+})
+
+export default connect(mapStateToProps)(CartMenu)
